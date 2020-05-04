ae967c76d5b227bb7aaa25a1255215577b13978f
iEnemY <i@ienemy.ru>
2020-05-04 13:47:40 +0300

    build script updated. added pack only flag.

--- build.bat
+++ build.bat
@@ -11,6 +11,9 @@ rem Path like X:\tools\depbo\ - and there are must lay depbo.exe
 set PBO_TOOL=%PROJECT_DRIVE%:\tools\AddonBuilder
 set TOOL_EXECUTABLE=AddonBuilder.exe
 
+rem Clear cache
+rd /S /Q C:\Users\%USERNAME%\AppData\Local\Temp\core
+
 rem Addon will placed in X:\pbo\ifatigue by default
 mkdir "%PROJECT_DRIVE%:\%OUTPUT%\%ADDON_NAME%"
 mkdir "%PROJECT_DRIVE%:\%OUTPUT%\%ADDON_NAME%\addons"
@@ -37,6 +40,6 @@ cls
 
 cd /d "%PBO_TOOL%"
 rem %TOOL_EXECUTABLE% "%SOURCE_DRIVE%:\%SOURCES_DIR%\%ADDON_NAME%\fatigue\addons\core" "%PROJECT_DRIVE%:\%OUTPUT%\%ADDON_NAME%\addons\%ADDON_NAME%_core.pbo" -prefix "x\fatigue\addons\core"
-%TOOL_EXECUTABLE% "%SOURCE_DRIVE%:\%SOURCES_DIR%\%ADDON_NAME%\fatigue\addons\core" "%PROJECT_DRIVE%:\%OUTPUT%\%ADDON_NAME%\addons" -prefix="x\fatigue\addons\core" -sign="%KEY%"
-%TOOL_EXECUTABLE% "%SOURCE_DRIVE%:\%SOURCES_DIR%\%ADDON_NAME%\cba\addons\settings_userconfig" "%PROJECT_DRIVE%:\%OUTPUT%\%ADDON_NAME%\addons" -prefix="cba_settings_userconfig" -sign="%KEY%"
+%TOOL_EXECUTABLE% "%SOURCE_DRIVE%:\%SOURCES_DIR%\%ADDON_NAME%\fatigue\addons\core" "%PROJECT_DRIVE%:\%OUTPUT%\%ADDON_NAME%\addons" -prefix="x\fatigue\addons\core" -packonly -sign="%KEY%"
+%TOOL_EXECUTABLE% "%SOURCE_DRIVE%:\%SOURCES_DIR%\%ADDON_NAME%\cba\addons\settings_userconfig" "%PROJECT_DRIVE%:\%OUTPUT%\%ADDON_NAME%\addons" -prefix="cba_settings_userconfig" -packonly -sign="%KEY%"
 cd /d "%PROJECT_DRIVE%:\%INPUT%"
